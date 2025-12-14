0f0555211564496c07f837c335c6ef1fa9f53c96
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-12-14 21:02:19 +0530

    imports fixed

--- src/components/dashboard/AssetAllocationNested.jsx
+++ src/components/dashboard/AssetAllocationNested.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import { motion } from 'motion/react';
 import ReactECharts from 'echarts-for-react';
 import { FiPieChart } from 'react-icons/fi';
 
